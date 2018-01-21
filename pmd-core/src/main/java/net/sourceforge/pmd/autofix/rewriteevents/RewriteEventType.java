03cf810f830d4bb1898fa203ab59fcc16121c145
Matias Comercio <mcomerciovazquez@gmail.com>
2018-01-21 18:57:37 -0300

    Add RewriteEventTypeTest

--- pmd-core/src/main/java/net/sourceforge/pmd/autofix/rewriteevents/RewriteEventType.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/autofix/rewriteevents/RewriteEventType.java
@@ -4,6 +4,9 @@
 
 package net.sourceforge.pmd.autofix.rewriteevents;
 
+// xnow document; add clarification saying that index is for
+//  adding the possibility of putting these event types in arrays
+//  for quicker access (sth like that)
 public enum RewriteEventType {
     INSERT(0), REMOVE(1), REPLACE(2);
 
---
+++ pmd-core/src/test/java/net/sourceforge/pmd/autofix/rewriteevents/RewriteEventTypeTest.java
@@ -0,0 +1,25 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.autofix.rewriteevents;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class RewriteEventTypeTest {
+
+    @Test
+    public void testValidIndexes() {
+        final RewriteEventType[] rewriteEventTypes = new RewriteEventType[RewriteEventType.values().length];
+        for (final RewriteEventType rewriteEventType : RewriteEventType.values()) {
+            final int index = rewriteEventType.getIndex();
+            // If IndexOutOfBoundException, index is equal or greater than RewriteEventType.values().length,
+            //  which is an invalid condition for this enum specification
+            if (rewriteEventTypes[index] != null) {
+                fail(); // There was another rewrite event with the same index
+            }
+            rewriteEventTypes[index] = rewriteEventType;
+        }
+    }
+}
